364caf6b9e489edac0e7b028066ca81218f6a353
Subanesh-swe <subaneshtechie@gmail.com>
2022-11-24 14:50:06 +0530

    edit req

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 mongoose.connection.once('open', () => console.log('connected to the database'));
 
 const PORT = process.env.PORT || 8081;
